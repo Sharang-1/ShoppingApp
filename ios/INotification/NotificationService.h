3d502cd7dbc6c05e96994e4cf6098dcb912b229e
Anushil Sutaria <anushilsutaria@Anushils-MacBook-Pro.local>
2021-02-08 21:11:57 +0530

    Till now

---
+++ ios/INotification/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  INotification
+//
+//  Created by Anushil Sutaria on 06/02/21.
+//  Copyright © 2021 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
