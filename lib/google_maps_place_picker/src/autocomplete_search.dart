ba8954640d8923dd46a659d7e4cd2ba5938bb873
Yash Chudasama <yashchudasama1722@gmail.com>
2021-02-11 18:12:42 +0530

    fixed place-picker issue

--- lib/google_maps_place_picker/src/autocomplete_search.dart
+++ lib/google_maps_place_picker/src/autocomplete_search.dart
@@ -228,7 +228,7 @@ class AutoCompleteSearchState extends State<AutoCompleteSearch> {
 
     overlayEntry = OverlayEntry(
       builder: (context) => Positioned(
-        top: appBarRenderBox.size.height + 20,
+        top: appBarRenderBox.size.height + MediaQuery.of(context).padding.top,
         left: screenWidth * 0.025,
         right: screenWidth * 0.025,
         child: Material(
