bf2addd481eac081084038e52bf80bd358b64510
Abhishek <abhi@Abhisheks-MacBook-Pro.local>
2022-03-29 12:45:41 +0530

    updated version

--- lib/constants/server_urls.dart
+++ lib/constants/server_urls.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 // kReleaseMode
 
-const bool releaseMode = true;
+const bool releaseMode = kReleaseMode;
 
 const String BASE_URL =
     releaseMode ? "https://dzor.in/api/" : "https://dev.dzor.in/api/";
